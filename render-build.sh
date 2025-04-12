3a08c99edf2db4f3036d7357d1e13cc7bcaba52d
Erick <piuratumbes2018@gmail.com>
2025-04-12 18:53:44 -0500

    Update render-build.sh and fix requirements.txt

---
+++ render-build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Actualizar paquetes e instalar herramientas adicionales necesarias
+apt-get update && apt-get install -y \
+    build-essential \
+    libffi-dev \
+    libssl-dev \
+    libxml2-dev \
+    libxslt1-dev \
+    libjpeg-dev \
+    zlib1g-dev \
+    tesseract-ocr \
+    libtesseract-dev
+
+# Actualizar pip antes de instalar dependencias
+pip install --upgrade pip
